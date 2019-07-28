2c3b99e3b5d97bb02e312fe07f3be45a3369d9f7
Martin Pham <tuyentq2009@gmail.com>
2019-07-28 20:34:03 +0200

    template variable

--- src/main/java/com/mp/todo/web/TaskController.java
+++ src/main/java/com/mp/todo/web/TaskController.java
@@ -3,13 +3,15 @@ package com.mp.todo.web;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class TaskController {
     @RequestMapping("/task")
-    public String index()
+    public String index(Model model)
     {
+        model.addAttribute("name", "Martin");
         return "web/task/index";
     }
 }
