b4d95a3ff243ad6f98a4c70f1e4afd32864f996b
Martin Pham <tuyentq2009@gmail.com>
2019-07-28 20:36:04 +0200

    request variable

--- src/main/java/com/mp/todo/web/TaskController.java
+++ src/main/java/com/mp/todo/web/TaskController.java
@@ -4,14 +4,15 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class TaskController {
-    @RequestMapping("/task")
-    public String index(Model model)
+    @GetMapping("/task")
+    public String index(Model model, @RequestParam("name") String name)
     {
-        model.addAttribute("name", "Martin");
+        model.addAttribute("name", name);
         return "web/task/index";
     }
 }
